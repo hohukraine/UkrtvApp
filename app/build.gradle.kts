c4b2c10747d129d750c8a0159c24cce71774db15
hohukraine <ananevaleksandr51@gmail.com>
2026-08-15 09:21:47 +0300

    Release v1.3.3 (versionCode 10)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -25,8 +25,8 @@ android {
         applicationId = "ua.ukrtv.app"
         minSdk = 26
         targetSdk = 37
-        versionCode = 9
-        versionName = "1.3.2"
+        versionCode = 10
+        versionName = "1.3.3"
         buildConfigField("String", "UPDATE_URL", "\"https://raw.githubusercontent.com/hohukraine/UkrtvApp/main/update.json\"")
         buildConfigField("String", "TMDB_API_KEY", "\"$tmdbApiKey\"")
     }
